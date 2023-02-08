c6aa859ae772fdf1157e1725759467ec1ca597fb
Kirill Dzhumanov <Djum83@mail.ru>
2023-02-08 18:56:03 +0300

    init: question

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsConfigOpen = ['(', '{', '[','|' , '1', '3', '5', '7', '8'];
+  const bracketsConfigPair = {
+    [')']: '(',
+    ['}']: '{',
+    ['[']: ']',
+    ['|']: '|',
+    ['1']: '2',
+    ['3']: '4',
+    ['5']: '6',
+    ['7']: '7',
+    ['8']: '8',
+  };
+
+  function isBracketsOk(str) {
+    str = '([{}])';
+    let stack =[];
+    for (let i = 0; i < str.length; i++) {
+      let currentBracket = str[i];
+      if (bracketsConfigOpen.includes(currentBracket)) {
+        stack.push(currentBracket);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+        let topBracket = stack[stack.length - 1];
+        if (bracketsConfigPair[currentBracket] === topBracket) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
+  }
+  console.log(isBracketsOk(str));
 }
+
+
+ 
